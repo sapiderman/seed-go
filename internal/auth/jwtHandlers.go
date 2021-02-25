c73114fb631fcde26cdfeb6c0fcddcd060821d09
Budhi <sapiderman.lumpat@gmail.com>
2021-02-26 00:56:42 +0700

    add: parse refreesh token

--- internal/auth/jwtHandlers.go
+++ internal/auth/jwtHandlers.go
@@ -82,7 +82,7 @@ func ExtractToken(r *http.Request) string {
 	return ""
 }
 
-// VerifyAccessToken
+// VerifyAccessToken ..
 func VerifyAccessToken(r *http.Request) (*jwt.Token, error) {
 	tokenString := ExtractToken(r)
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
@@ -98,6 +98,7 @@ func VerifyAccessToken(r *http.Request) (*jwt.Token, error) {
 	return token, nil
 }
 
+// TokenValid ...
 func TokenValid(r *http.Request) error {
 	token, err := VerifyAccessToken(r)
 	if err != nil {
@@ -107,4 +108,67 @@ func TokenValid(r *http.Request) error {
 		return err
 	}
 	return nil
+}
+
+// RefreshTokens ...
+func RefreshTokens(refreshToken string) (string, string, error) {
+	logf := jwtLog.WithField("func", "RefreshTokens")
+
+	//verify the token
+	rtKey := config.Get("jwt.refreshKey")
+	token, err := jwt.Parse(refreshToken, func(token *jwt.Token) (interface{}, error) {
+		//Make sure that the token method conform to "SigningMethodHMAC"
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			logf.Error("unexpeted signing key")
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(rtKey), nil
+	})
+	//if there is an error, the token must have expired
+	if err != nil {
+		logf.Error(err)
+		return "", "", err
+	}
+	//is token valid?
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		logf.Error(err)
+		return "", "", err
+	}
+	//Since token is valid, get the uuid:
+	claims, ok := token.Claims.(jwt.MapClaims) //the token claims should conform to MapClaims
+	if ok && token.Valid {
+		subject, ok := claims["sub"].(string) //convert the interface to string
+		if !ok {
+			logf.Error(err)
+			return "", "", err
+		}
+
+		// userId, err := strconv.ParseUint(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+		// if err != nil {
+		// 	c.JSON(http.StatusUnprocessableEntity, "Error occurred")
+		// 	return
+		// }
+		//Delete the previous Refresh Token
+		// deleted, delErr := DeleteAuth(refreshUuid)
+		// if delErr != nil || deleted == 0 { //if any goes wrong
+		// 	logf.Error(delErr)
+		// 	return "", "", delErr
+		// }
+		//Create new pairs of refresh and access tokens
+		at, _, createErr := GenerateTokenPair(subject)
+		if err != nil {
+			logf.Error(createErr)
+			return "", "", createErr
+		}
+		//save the tokens metadata to redis
+		// saveErr := CreateAuth(userId, ts)
+		// if saveErr != nil {
+		// 	c.JSON(http.StatusForbidden, saveErr.Error())
+		// 	return
+		// }
+
+		return at, refreshToken, nil
+	}
+	logf.Error("refresh expired")
+	return "", "", nil
 }
