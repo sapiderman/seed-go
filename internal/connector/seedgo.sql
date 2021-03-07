0616cca95f4d6d16e1d692b7616f931b4241810d
Budhi <sapiderman.lumpat@gmail.com>
2021-03-07 23:37:27 +0700

    hmm adding a table

--- internal/connector/seedgo.sql
+++ internal/connector/seedgo.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) UNIQUE NOT NULL,
     phone VARCHAR UNIQUE NOT NULL, 
     email VARCHAR(100) UNIQUE NOT NULL ,
+    verified BOOLEAN DEFAULT false,
     password VARCHAR(255) NOT NULL,
     pin INT,
     device INT REFERENCES device(id)
@@ -22,11 +23,22 @@ CREATE TABLE IF NOT EXISTS devices (
     push_notif_id VARCHAR,
     device_id VARCHAR
 );
+CREATE TABLE IF NOT EXISTS articles (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
+    article TEXT,
+    author INT REFERENCES users(id)
+
+)
 CREATE TABLE IF NOT EXISTS login_history (
     id SERIAL PRIMARY KEY,
-        created_at TIMESTAMPTZ,
-        username VARCHAR,
-        os VARCHAR,
-        browser VARCHAR,
+    created_at TIMESTAMPTZ,
+    username VARCHAR,
+    os VARCHAR,
+    browser VARCHAR,
+        updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
 )
 
